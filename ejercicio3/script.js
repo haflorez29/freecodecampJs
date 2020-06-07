2b0f4d7ca9e39f9e6ff361b74fecb9717d9e12c3
haflorez29 <heidyflorez55@gmail.com>
2020-06-07 04:06:34 -0500

    exercise3 finall

--- ejercicio3/script.js
+++ ejercicio3/script.js
@@ -12,6 +12,10 @@ var codigo = [
   {letra: "K", cifrado: "X"},
   {letra: "L", cifrado: "Y"},
   {letra: "M", cifrado: "Z"},
+  {letra: " ", cifrado: " "},
+  {letra: "!", cifrado: "!"},
+  {letra: "?", cifrado: "?"},
+  {letra: ".", cifrado: "."},
 ];
 //asi encuentro la propiedad del objeto
 // console.log (codigo[1].cifrado);
@@ -21,21 +25,31 @@ function rot13 (str) {
   str = str.toUpperCase();
   //divido el string ingresado en un arreglo
   var newArray = str.split("");
+ 
   // console.log (newArray)  
   //reviso si el el string ingresado esta en el arreglo y retorno el indice 
+  var bucle = newArray.map (function (str){  
   var i = codigo.findIndex(element => element.letra == str || element.cifrado == str );
   // console.log("El elemento buscado está en el índice ", i);  
   var letra = codigo[i].letra;
+  // console.log(letra)
   var cifrado = codigo[i].cifrado;
+  // console.log(cifrado)
+  // console.log (letra)
 // recorro el arreglo que ingresa y que me evalue las condiciones para que retorne el cifrado
   // newArray.forEach ((element) =>{    
-  if ( newArray == letra){
-  return (cifrado)
-  }else if( newArray == cifrado){
+  if ( str === letra ){
+  return (cifrado )
+  }else if( str === cifrado){
     return (letra)
+  }else{
+    return 'no existe'
   } 
+
+})
+return bucle.join("")
 // })
 }
-console.log (rot13("z"))
+console.log (rot13("SER RPBQRPNZC!"))
 // no supe como hacer para que al ser mas de una letra me recorra ese arreglo y realize el if en cada uno
 
